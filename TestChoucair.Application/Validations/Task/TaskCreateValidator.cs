9859b2869bd2873055e51a8a01d4f7b807786dc4
sergio orjuela <checho@sergios-Mac-mini.local>
2024-05-19 18:54:31 -0500

    validator time

--- TestChoucair.Application/Validations/Task/TaskCreateValidator.cs
+++ TestChoucair.Application/Validations/Task/TaskCreateValidator.cs
@@ -7,6 +7,15 @@ namespace TestChoucair.Application.Validator
     {
         public TaskCreateValidator()
         {
+            RuleFor(x => x.Title)
+            .NotEmpty().WithMessage("Title is required.");
+            
+            RuleFor(x => x.Description)
+            .NotEmpty().WithMessage("Description is required.");
+
+             RuleFor(x => x.Time)
+            .NotEmpty().WithMessage("Time is required.");
+
         }
     }
 }
